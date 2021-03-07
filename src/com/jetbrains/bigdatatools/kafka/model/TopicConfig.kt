40d5cb16f34cd8014cb0584a3275a84e38e10b68
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2021-03-07 12:55:49 +0300

    Kafka(refactor): Extract common AbstractTopicDetailController
    GitOrigin-RevId: 7e374e765c286e54edbb7fc8e3d74c410837543d

--- src/com/jetbrains/bigdatatools/kafka/model/TopicConfig.kt
+++ src/com/jetbrains/bigdatatools/kafka/model/TopicConfig.kt
@@ -6,10 +6,10 @@ import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.jvm.javaField
 
-class TopicConfigPresentable(val name: String, val value: String, val defaultValue: String = "") : RemoteInfo {
+class TopicConfig(val name: String, val value: String, val defaultValue: String = "") : RemoteInfo {
   companion object {
-    val renderableColumns: List<KProperty1<TopicConfigPresentable, *>> by lazy {
-      TopicConfigPresentable::class.declaredMemberProperties.filter { DataRenderingUtil.shouldRenderFrom(it.javaField?.annotations) }
+    val renderableColumns: List<KProperty1<TopicConfig, *>> by lazy {
+      TopicConfig::class.declaredMemberProperties.filter { DataRenderingUtil.shouldRenderFrom(it.javaField?.annotations) }
     }
   }
 }
