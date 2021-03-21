b96200b068bad2a885352dd4333fedcc8dffc6fe
Confluent Eng <dtx-eng@confluent.io>
2021-03-22 01:28:56 +0300

    Monitoring(refactor): Extract common monitoring classes
    GitOrigin-RevId: 672f945fd5e17787c8df744598a3f4cc1bd35773

--- src/com/jetbrains/bigdatatools/kafka/toolwindow/controllers/TopicPartitionsController.kt
+++ src/com/jetbrains/bigdatatools/kafka/toolwindow/controllers/TopicPartitionsController.kt
@@ -3,11 +3,16 @@ package com.jetbrains.bigdatatools.kafka.toolwindow.controllers
 import com.jetbrains.bigdatatools.kafka.data.KafkaDataManager
 import com.jetbrains.bigdatatools.kafka.model.TopicPartition
 import com.jetbrains.bigdatatools.kafka.toolwindow.config.KafkaToolWindowSettings
+import com.jetbrains.bigdatatools.monitoring.toolwindow.DetailsTableMonitoringController
 
-class TopicPartitionsController(private val dataManager: KafkaDataManager) : AbstractTopicDetailController<TopicPartition>() {
+class TopicPartitionsController(private val dataManager: KafkaDataManager) : DetailsTableMonitoringController<TopicPartition>() {
+  init {
+    init()
+  }
+
   override fun getColumnSettings() = KafkaToolWindowSettings.getInstance().topicPartitionsColumnSettings
 
   override fun getRenderableColumns() = TopicPartition.renderableColumns
 
-  override fun getModel(topicId: String) = dataManager.getTopicPartitionsModel(topicId)
+  override fun getDataModel() = selectedId?.let { dataManager.getTopicPartitionsModel(it) }
 }
