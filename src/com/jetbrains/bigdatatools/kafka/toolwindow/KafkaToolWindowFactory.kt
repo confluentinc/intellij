9bb45c6ebc6826987cb6a000c79af9316f3a73a3
Nikita Pavlenko <Nikita.Pavlenko@jetbrains.com>
2021-04-28 17:27:58 +0300

    Common(fix): Spark, Hadoop, Databricks and Kafka toolwindows will be inactive until first connection of their type is created. #BDIDE-1775 Fixed
    GitOrigin-RevId: fc46a04d9182792cbe4a5ff4ef9b29944388c297

--- src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaToolWindowFactory.kt
+++ src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaToolWindowFactory.kt
@@ -4,6 +4,10 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
+import com.jetbrains.bigdatatools.kafka.util.KafkaMessagesBundle
+import com.jetbrains.bigdatatools.monitoring.toolwindow.ToolWindowActivator
+import com.jetbrains.bigdatatools.settings.manager.RfsConnectionDataManager
 
 class KafkaToolWindowFactory : ToolWindowFactory, DumbAware {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
@@ -11,6 +15,14 @@ class KafkaToolWindowFactory : ToolWindowFactory, DumbAware {
   }
 
   override fun init(toolWindow: ToolWindow) {
-    toolWindow.stripeTitle = "Kafka"
+    toolWindow.stripeTitle = KafkaMessagesBundle.message("toolwindow.title")
+
+    if (!toolWindow.isAvailable) {
+      ToolWindowActivator(KafkaMonitoringToolWindowController.TOOL_WINDOW_ID, KafkaConnectionData::class.java)
+    }
+  }
+
+  override fun shouldBeAvailable(project: Project): Boolean {
+    return !RfsConnectionDataManager.instance?.getTyped<KafkaConnectionData>(project).isNullOrEmpty()
   }
 }
