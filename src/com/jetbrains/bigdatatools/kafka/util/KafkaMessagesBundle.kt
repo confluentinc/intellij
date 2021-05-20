d5dbcbaabc515412d766578279eb07b3b608244c
Confluent Eng <dtx-eng@confluent.io>
2021-05-20 11:58:34 +0300

    Common(fix): All message bundles switched to DynamicBundle.
    GitOrigin-RevId: 26c8a6bb50653d9f218e76c564b86b0f7fa24c24

--- src/com/jetbrains/bigdatatools/kafka/util/KafkaMessagesBundle.kt
+++ src/com/jetbrains/bigdatatools/kafka/util/KafkaMessagesBundle.kt
@@ -1,26 +1,15 @@
 package com.jetbrains.bigdatatools.kafka.util
 
-import com.intellij.AbstractBundle
-import com.intellij.reference.SoftReference
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import java.lang.ref.Reference
-import java.util.*
 
-object KafkaMessagesBundle {
-  private const val BUNDLE_NAME = "messages.kafka"
-  private var ourBundle: Reference<ResourceBundle?>? = null
+@NonNls
+private const val BUNDLE = "messages.kafka"
 
-  private val bundle: ResourceBundle?
-    get() {
-      var bundle = SoftReference.dereference(ourBundle)
-      if (bundle == null) {
-        bundle = ResourceBundle.getBundle(BUNDLE_NAME)
-        ourBundle = java.lang.ref.SoftReference(bundle)
-      }
-      return bundle
-    }
-
-  fun message(@PropertyKey(resourceBundle = BUNDLE_NAME) key: String, vararg params: Any): String {
-    return AbstractBundle.message(bundle!!, key, *params)
-  }
+object KafkaMessagesBundle : DynamicBundle(BUNDLE) {
+  @Nls
+  @JvmStatic
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
 }
