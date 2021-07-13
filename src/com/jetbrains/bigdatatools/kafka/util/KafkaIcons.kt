fc2fbaf0c71d2f48707a85c060790694da6a11cb
Nikita Pavlenko <Nikita.Pavlenko@jetbrains.com>
2021-07-13 20:16:15 +0300

    Common (refactor): Scheduled for removal call of IconLoader.getIcon replaced.
    GitOrigin-RevId: 8c38cb069be58ec59dcb5dc32c9de05b3fbde690

--- src/com/jetbrains/bigdatatools/kafka/util/KafkaIcons.kt
+++ src/com/jetbrains/bigdatatools/kafka/util/KafkaIcons.kt
@@ -3,5 +3,5 @@ package com.jetbrains.bigdatatools.kafka.util
 import com.intellij.openapi.util.IconLoader
 
 object KafkaIcons {
-  val MAIN_ICON = IconLoader.getIcon("/icons/kafka.svg")
+  val MAIN_ICON = IconLoader.getIcon("/icons/kafka.svg", this::class.java)
 }
