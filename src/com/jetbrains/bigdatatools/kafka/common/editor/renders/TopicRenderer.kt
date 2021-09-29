4a347b50d0d0c7e47304a2fb629b91d1dea4bf22
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2021-09-29 13:33:43 +0300

    Kafka(fix): Update topic in consumer/producer with dataManager
    GitOrigin-RevId: 26b9dd001c7e5ea4c621e71b2a85fe7213157e1a

--- src/com/jetbrains/bigdatatools/kafka/common/editor/renders/TopicRenderer.kt
+++ src/com/jetbrains/bigdatatools/kafka/common/editor/renders/TopicRenderer.kt
@@ -1,6 +1,6 @@
 package com.jetbrains.bigdatatools.kafka.common.editor.renders
 
-import com.jetbrains.bigdatatools.kafka.model.TopicPresentable
+import com.jetbrains.bigdatatools.kafka.common.models.TopicInEditor
 import javax.swing.DefaultListCellRenderer
 import javax.swing.JList
 
@@ -8,7 +8,7 @@ class TopicRenderer : DefaultListCellRenderer() {
   override fun getListCellRendererComponent(list: JList<*>?, value: Any?, index: Int,
                                             isSelected: Boolean, cellHasFocus: Boolean) =
     super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus).apply {
-      val topicPresentable = value as? TopicPresentable ?: return@apply
+      val topicPresentable = value as? TopicInEditor ?: return@apply
       @Suppress("HardCodedStringLiteral")
       text = topicPresentable.name
     }
