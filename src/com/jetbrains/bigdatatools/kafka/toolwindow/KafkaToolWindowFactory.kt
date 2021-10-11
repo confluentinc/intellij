43ecce6380f409ada981e8246a7d12d16e2044a6
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2021-10-11 11:08:49 +0300

    Monitoring(refactor): Continue refactor MonitoringToolWindowController 4
    GitOrigin-RevId: 90a8ae66887d52bdf9ee0307c4f4506ee72eedf3

--- src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaToolWindowFactory.kt
+++ src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaToolWindowFactory.kt
@@ -1,28 +1,17 @@
 package com.jetbrains.bigdatatools.kafka.toolwindow
 
-import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ToolWindowFactory
-import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
+import com.jetbrains.bigdatatools.kafka.settings.KafkaSettingsIds
 import com.jetbrains.bigdatatools.kafka.util.KafkaMessagesBundle
-import com.jetbrains.bigdatatools.monitoring.toolwindow.ToolWindowActivator
-import com.jetbrains.bigdatatools.settings.manager.RfsConnectionDataManager
+import com.jetbrains.bigdatatools.monitoring.toolwindow.MonitoringToolWindowFactory
 
-class KafkaToolWindowFactory : ToolWindowFactory, DumbAware {
+class KafkaToolWindowFactory : MonitoringToolWindowFactory() {
+  override val controllerId = KafkaMonitoringToolWindowController.TOOL_WINDOW_ID
+  override val connectionGroupId = KafkaSettingsIds.GROUP_ID
+  override val title: String = KafkaMessagesBundle.message("toolwindow.title")
+
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     KafkaMonitoringToolWindowController.getInstance(project)?.setUp(toolWindow)
-  }
-
-  override fun init(toolWindow: ToolWindow) {
-    toolWindow.stripeTitle = KafkaMessagesBundle.message("toolwindow.title")
-
-    if (!toolWindow.isAvailable) {
-      ToolWindowActivator(KafkaMonitoringToolWindowController.TOOL_WINDOW_ID, KafkaConnectionData::class.java)
-    }
-  }
-
-  override fun shouldBeAvailable(project: Project): Boolean {
-    return !RfsConnectionDataManager.instance?.getTyped<KafkaConnectionData>(project).isNullOrEmpty()
   }
 }
