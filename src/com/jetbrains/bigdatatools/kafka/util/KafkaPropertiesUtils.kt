190262b1c704a8564cb766dc55899378d978e7a1
Confluent Eng <dtx-eng@confluent.io>
2021-11-21 14:08:01 +0300

    Kafka(feat): Add code completion for properties field like in SparkSubmit #BDIDE-2438
    GitOrigin-RevId: a4cdbd73d87a5dc18d55bd8aa7755e1094c80760

---
+++ src/com/jetbrains/bigdatatools/kafka/util/KafkaPropertiesUtils.kt
@@ -0,0 +1,21 @@
+package com.jetbrains.bigdatatools.kafka.util
+
+import com.jetbrains.bigdatatools.ui.components.ConnectionProperty
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.common.config.ConfigDef.NO_DEFAULT_VALUE
+import java.util.*
+
+object KafkaPropertiesUtils {
+  fun getAdminPropertiesDescriptions() =
+    AdminClientConfig.configDef().configKeys().values.mapNotNull { configKey ->
+      ConnectionProperty(
+        propertyName = configKey.name,
+        default = if (configKey.defaultValue == NO_DEFAULT_VALUE) "" else configKey.defaultValue?.toString() ?: "null",
+        meaning = configKey.documentation,
+        rightSideInfo = configKey.type.name.lowercase().replaceFirstChar {
+          if (it.isLowerCase()) it.titlecase(Locale.getDefault())
+          else it.toString()
+        }
+      )
+    }
+}
