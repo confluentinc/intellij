fd91c123920ab6c08cd2511dda9ce5bc525c57c0
Confluent Eng <dtx-eng@confluent.io>
2021-12-13 16:07:27 +0300

    Common (cosmetic): Changed a bit, connection setting UI for S3 and Kafka. #BDIDE-2544 Fixed
    GitOrigin-RevId: 956406461c17cd540c8347c0da87911b2ff50e81

--- src/com/jetbrains/bigdatatools/kafka/rfs/KafkaPropertySource.kt
+++ src/com/jetbrains/bigdatatools/kafka/rfs/KafkaPropertySource.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.bigdatatools.kafka.rfs
 
 import com.jetbrains.bigdatatools.kafka.util.KafkaMessagesBundle
+import com.jetbrains.bigdatatools.settings.components.RenderableEntity
 
-enum class KafkaPropertySource(val title: String) {
+enum class KafkaPropertySource(override val title: String) : RenderableEntity {
   DIRECT(KafkaMessagesBundle.message("settings.property.source.direct")),
-  FILE(KafkaMessagesBundle.message("settings.property.source.file"))
+  FILE(KafkaMessagesBundle.message("settings.property.source.file"));
+
+  override val id = name.lowercase()
 }
