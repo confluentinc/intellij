8f63464d4618c5689b11b077ab48d1a3ad312b06
Nikita Ashihmin <nikita.ashihmin@jetbrains.com>
2022-01-20 11:29:55 +0300

    Common(task): Extract jackson to separate libs #BDIDE-2645
    GitOrigin-RevId: 0b08628f183dd68a1b67497be1dd528ab0e0c343

--- src/com/jetbrains/bigdatatools/kafka/model/InternalCompatibilityCheck.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.bigdatatools.kafka.model
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-class InternalCompatibilityCheck {
-  @JsonProperty("is_compatible")
-  private val isCompatible = false
-}
