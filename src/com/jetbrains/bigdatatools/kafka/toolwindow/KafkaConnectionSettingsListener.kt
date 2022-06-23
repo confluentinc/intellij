3698736443f41d931fd26c1410bec07ea7a1265f
Nikita Pavlenko <Nikita.Pavlenko@jetbrains.com>
2022-06-23 18:12:52 +0200

    Kafka (fix): Kafka Producer and Consumer editor tabs will be closed on connection change. #BDIDE-3050
    GitOrigin-RevId: 7d746c45b5cb00b2395afb1ba5040f3879e21ca8

---
+++ src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaConnectionSettingsListener.kt
@@ -0,0 +1,25 @@
+package com.jetbrains.bigdatatools.kafka.toolwindow
+
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.jetbrains.bigdatatools.kafka.common.editor.KafkaEditorProvider
+import com.jetbrains.bigdatatools.settings.ConnectionSettingsListener
+import com.jetbrains.bigdatatools.settings.ModificationKey
+import com.jetbrains.bigdatatools.settings.connections.ConnectionData
+
+class KafkaConnectionSettingsListener : ConnectionSettingsListener {
+  override fun onConnectionModified(project: Project?, connectionData: ConnectionData, modified: Collection<ModificationKey>) {
+
+    val projects = if (project == null) ProjectManager.getInstance().openProjects else arrayOf(project)
+    projects.forEach { prj ->
+      val editorManager = FileEditorManagerEx.getInstanceEx(prj)
+      editorManager.openFiles.toList().forEach { file ->
+        val dataManager = file.getUserData(KafkaEditorProvider.KAFKA_MANAGER_KEY) ?: return
+        if (dataManager.connectionId != connectionData.innerId) return
+        val window = editorManager.windows.find { it.isFileOpen(file) } ?: return
+        editorManager.closeFile(file, window)
+      }
+    }
+  }
+}
