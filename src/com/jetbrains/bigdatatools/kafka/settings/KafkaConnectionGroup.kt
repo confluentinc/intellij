aff266c42b5b4b6c58c81b5a885c04438a50735e
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2022-11-11 23:03:21 +0400

    [bdt] rfs(refactor): use type parameters for ConnectionData factories, separate factories from groups
    GitOrigin-RevId: d5ff8d8309604600c06a4c2b07297af2931d17d9

--- src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionGroup.kt
+++ src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionGroup.kt
@@ -1,17 +1,18 @@
 package com.jetbrains.bigdatatools.kafka.settings
 
-import com.jetbrains.bigdatatools.common.settings.connections.ConnectionData
-import com.jetbrains.bigdatatools.common.settings.connections.ConnectionGroup
+import com.jetbrains.bigdatatools.common.settings.connections.ConnectionFactory
 import com.jetbrains.bigdatatools.common.settings.connections.MonitoringConnectionGroup
 import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
 import com.jetbrains.bigdatatools.kafka.util.KafkaMessagesBundle
 import icons.BigdatatoolsKafkaIcons
 
-class KafkaConnectionGroup : ConnectionGroup(id = KafkaSettingsIds.GROUP_ID,
-                                             name = KafkaMessagesBundle.message("settings.group.name"),
-                                             icon = BigdatatoolsKafkaIcons.Kafka,
-                                             parentGroupId = MonitoringConnectionGroup.GROUP_ID) {
-  override fun newData(): ConnectionData = KafkaConnectionData().apply {
+class KafkaConnectionGroup : ConnectionFactory<KafkaConnectionData>(
+  id = KafkaSettingsIds.GROUP_ID,
+  name = KafkaMessagesBundle.message("settings.group.name"),
+  icon = BigdatatoolsKafkaIcons.Kafka,
+  parentGroupId = MonitoringConnectionGroup.GROUP_ID
+) {
+  override fun newData() = KafkaConnectionData().apply {
     name = "Kafka connection"
     uri = "127.0.0.1:9092"
   }
