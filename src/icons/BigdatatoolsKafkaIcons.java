aad4bb203a128c2ffea2b3799608987101f1f106
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-27 09:01:26 +0100

    use XXH3 instead of MurMur3 to generate an icon image key
    The main reason — use XXH3 everywhere
    
    GitOrigin-RevId: 7b158b9b3689d16f09182abb0b58113c7303f874

--- src/icons/BigdatatoolsKafkaIcons.java
+++ src/icons/BigdatatoolsKafkaIcons.java
@@ -13,8 +13,8 @@ public final class BigdatatoolsKafkaIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, BigdatatoolsKafkaIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Bookmark_off = load("icons/bookmark_off.svg", -1390226446, 0);
-  /** 16x16 */ public static final @NotNull Icon Bookmark_on = load("icons/bookmark_on.svg", 1075852510, 0);
-  /** 16x16 */ public static final @NotNull Icon Kafka = load("icons/kafka.svg", 174041856, 0);
-  /** 13x13 */ public static final @NotNull Icon KafkaToolWindow = load("icons/kafkaToolWindow.svg", -1674776211, 2);
+  /** 16x16 */ public static final @NotNull Icon Bookmark_off = load("icons/bookmark_off.svg", 2071958187, 0);
+  /** 16x16 */ public static final @NotNull Icon Bookmark_on = load("icons/bookmark_on.svg", 1739320840, 0);
+  /** 16x16 */ public static final @NotNull Icon Kafka = load("icons/kafka.svg", 530310811, 0);
+  /** 13x13 */ public static final @NotNull Icon KafkaToolWindow = load("icons/kafkaToolWindow.svg", 2045665598, 2);
 }
