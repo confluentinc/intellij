2f804290c5becb5c499b9972c905aa2f9ba98cb2
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2023-01-09 19:57:42 +0400

    [bdt] BDIDE-3750 Google Cloud(feat): Implement GCloud oauth support
    GitOrigin-RevId: 1648c656921eb53f7f7ae336883c95ccdbd67422

--- src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionConfigurable.kt
+++ src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionConfigurable.kt
@@ -1,31 +1,15 @@
 package com.jetbrains.bigdatatools.kafka.settings
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
-import com.jetbrains.bigdatatools.kafka.client.KafkaConnectionChecker
-import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
 import com.jetbrains.bigdatatools.common.settings.connections.ConnectionConfigurable
-import com.jetbrains.bigdatatools.common.settings.connections.ConnectionTesting
-import com.jetbrains.bigdatatools.common.settings.defaultui.ConnectionError
-import com.jetbrains.bigdatatools.common.settings.defaultui.ConnectionStatus
-import com.jetbrains.bigdatatools.common.settings.defaultui.ConnectionSuccessful
+import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
 import icons.BigdatatoolsKafkaIcons
 
 class KafkaConnectionConfigurable(connectionData: KafkaConnectionData, project: Project) :
   ConnectionConfigurable<KafkaConnectionData>(connectionData, project, BigdatatoolsKafkaIcons.Kafka) {
-  override fun createSettingsCustomizer() = KafkaSettingsCustomizer(project, connectionData, disposable)
+  private val settingsCustomizer: KafkaSettingsCustomizer
+    get() = KafkaSettingsCustomizer(project, connectionData, disposable)
 
-  override fun createConnectionTesting(): ConnectionTesting<KafkaConnectionData> = object : ConnectionTesting<KafkaConnectionData> {
-    override fun testConnection(conn: KafkaConnectionData,
-                                testDisposable: Disposable,
-                                callback: (ConnectionStatus) -> Unit) {
-      val error = KafkaConnectionChecker.checkConnection(conn, testDisposable)
-      if (error == null) {
-        callback(ConnectionSuccessful())
-      }
-      else {
-        callback(ConnectionError(error.cause ?: error))
-      }
-    }
-  }
+  override fun createSettingsCustomizer() = settingsCustomizer
+  override fun createConnectionTesting() = KafkaTestingBase(project, settingsCustomizer)
 }
