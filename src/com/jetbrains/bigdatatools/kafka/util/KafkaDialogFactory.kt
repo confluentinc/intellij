ffa61c3f377b12cac8f66214da4f5f22949944ad
Confluent Eng <dtx-eng@confluent.io>
2023-01-12 17:46:42 +0100

    [bdt] Kafka (refactor): Removed MigLayout, added default values in CreateTopicDialog.
    GitOrigin-RevId: bbf8c276a6271abf9b9fd12d9b0f3f7c34da7c87

--- src/com/jetbrains/bigdatatools/kafka/util/KafkaDialogFactory.kt
+++ src/com/jetbrains/bigdatatools/kafka/util/KafkaDialogFactory.kt
@@ -2,13 +2,14 @@ package com.jetbrains.bigdatatools.kafka.util
 
 import com.intellij.openapi.ui.DialogBuilder
 import com.intellij.openapi.ui.InputValidatorEx
-import com.jetbrains.bigdatatools.kafka.data.KafkaDataManager
+import com.intellij.ui.dsl.builder.AlignX
+import com.intellij.ui.dsl.builder.panel
 import com.jetbrains.bigdatatools.common.settings.buildValidator
 import com.jetbrains.bigdatatools.common.settings.registerValidator
 import com.jetbrains.bigdatatools.common.settings.withNumberOrEmptyValidator
-import com.jetbrains.bigdatatools.common.ui.MigPanel
 import com.jetbrains.bigdatatools.common.ui.doOnChange
 import com.jetbrains.bigdatatools.common.util.MessagesBundle
+import com.jetbrains.bigdatatools.kafka.data.KafkaDataManager
 import javax.swing.JTextField
 
 object KafkaDialogFactory {
@@ -19,13 +20,12 @@ object KafkaDialogFactory {
     builder.title(KafkaMessagesBundle.message("action.create.topic"))
     val nameField = JTextField("NewTopic", 15)
     nameField.selectAll()
-    val numPartition = JTextField(6).withNumberOrEmptyValidator(builder).also {
+    val numPartition = JTextField("3", 6).withNumberOrEmptyValidator(builder).also {
       it.toolTipText = KafkaMessagesBundle.message("create.topic.leave.empty.for.default")
     }
-    val replicationFactor = JTextField(6).withNumberOrEmptyValidator(builder).also {
+    val replicationFactor = JTextField("1", 6).withNumberOrEmptyValidator(builder).also {
       it.toolTipText = KafkaMessagesBundle.message("create.topic.leave.empty.for.default")
     }
-
 
     val validator = object : InputValidatorEx {
       override fun checkInput(inputString: String) = getErrorText(inputString) == null
@@ -40,10 +40,10 @@ object KafkaDialogFactory {
     registerValidator(builder, buildValidator(nameField, { nameField.text }, validator), nameField)
     nameField.doOnChange { builder.okActionEnabled(validator.canClose(nameField.text)) }
 
-    val centralPanel = MigPanel().apply {
-      row(KafkaMessagesBundle.message("dialog.create.topic.name"), nameField)
-      shortRow(KafkaMessagesBundle.message("dialog.create.topic.num.partition"), numPartition)
-      shortRow(KafkaMessagesBundle.message("dialog.create.topic.replication.factor"), replicationFactor)
+    val centralPanel = panel {
+      row(KafkaMessagesBundle.message("dialog.create.topic.name")) { cell(nameField).align(AlignX.FILL).resizableColumn() }
+      row(KafkaMessagesBundle.message("dialog.create.topic.num.partition")) { cell(numPartition) }
+      row(KafkaMessagesBundle.message("dialog.create.topic.replication.factor")) { cell(replicationFactor) }
     }
     builder.centerPanel(centralPanel)
 
