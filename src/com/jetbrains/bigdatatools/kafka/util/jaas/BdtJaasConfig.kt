057dc42f604d79e47d433fa6ecaa86854195e32c
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2023-01-28 16:36:48 +0400

    [bdt] BDIDE-3621 Kafka(feat): improve UI 2
    GitOrigin-RevId: 4a005f1a48d5741be4444a268f365a1d573e23fa

---
+++ src/com/jetbrains/bigdatatools/kafka/util/jaas/BdtJaasConfig.kt
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.bigdatatools.kafka.util.jaas
+
+import org.apache.kafka.common.KafkaException
+import java.io.IOException
+import java.io.StreamTokenizer
+import java.io.StringReader
+import javax.security.auth.login.AppConfigurationEntry
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag
+import javax.security.auth.login.Configuration
+
+/**
+ * Copy of JaasConfigs class
+ */
+class BdtJaasConfig(jaasConfigParams: String) : Configuration() {
+  var configEntries: MutableList<AppConfigurationEntry> = mutableListOf()
+    private set
+
+  val config: AppConfigurationEntry?
+    get() = configEntries.firstOrNull()
+
+  init {
+    val tokenizer = StreamTokenizer(StringReader(jaasConfigParams))
+    tokenizer.slashSlashComments(true)
+    tokenizer.slashStarComments(true)
+    tokenizer.wordChars('-'.code, '-'.code)
+    tokenizer.wordChars('_'.code, '_'.code)
+    tokenizer.wordChars('$'.code, '$'.code)
+    try {
+      while (tokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+        configEntries.add(parseAppConfigurationEntry(tokenizer))
+      }
+      require(!configEntries.isEmpty()) { "Login module not specified in JAAS config" }
+    }
+    catch (e: IOException) {
+      throw KafkaException("Unexpected exception while parsing JAAS config")
+    }
+  }
+
+  override fun getAppConfigurationEntry(name: String): Array<AppConfigurationEntry> {
+    return configEntries.toTypedArray()
+  }
+
+  private fun loginModuleControlFlag(flag: String?): LoginModuleControlFlag {
+    requireNotNull(flag) { "Login module control flag is not available in the JAAS config" }
+    val controlFlag: LoginModuleControlFlag = when (flag.uppercase()) {
+      "REQUIRED" -> LoginModuleControlFlag.REQUIRED
+      "REQUISITE" -> LoginModuleControlFlag.REQUISITE
+      "SUFFICIENT" -> LoginModuleControlFlag.SUFFICIENT
+      "OPTIONAL" -> LoginModuleControlFlag.OPTIONAL
+      else -> throw IllegalArgumentException("Invalid login module control flag '$flag' in JAAS config")
+    }
+    return controlFlag
+  }
+
+  @Throws(IOException::class)
+  private fun parseAppConfigurationEntry(tokenizer: StreamTokenizer): AppConfigurationEntry {
+    val loginModule = tokenizer.sval
+    require(tokenizer.nextToken() != StreamTokenizer.TT_EOF) { "Login module control flag not specified in JAAS config" }
+    val controlFlag = loginModuleControlFlag(tokenizer.sval)
+    val options: MutableMap<String, String?> = HashMap()
+    while (tokenizer.nextToken() != StreamTokenizer.TT_EOF && tokenizer.ttype != ';'.code) {
+      val key = tokenizer.sval
+      require(
+        !(tokenizer.nextToken() != '='.code || tokenizer.nextToken() == StreamTokenizer.TT_EOF || tokenizer.sval == null)) { "Value not specified for key '$key' in JAAS config" }
+      val value = tokenizer.sval
+      options[key] = value
+    }
+    require(tokenizer.ttype == ';'.code) { "JAAS config entry not terminated by semi-colon" }
+    return AppConfigurationEntry(loginModule, controlFlag, options)
+  }
+}
