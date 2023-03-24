5681490889b81a9a434576de3fbdde6a820d3805
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2023-03-24 20:21:57 +0300

    [bdt] refactor 'Test Connection' implementation
    GitOrigin-RevId: f8eefb534efeaf08294050666d98e7cd83f41e01

--- src/com/jetbrains/bigdatatools/kafka/settings/KafkaTestingBase.kt
+++ src/com/jetbrains/bigdatatools/kafka/settings/KafkaTestingBase.kt
@@ -5,6 +5,7 @@ import com.jetbrains.bigdatatools.common.rfs.settings.RfsConnectionTestingBase
 import com.jetbrains.bigdatatools.common.settings.defaultui.SettingsPanelCustomizer
 import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
 
-class KafkaTestingBase(project: Project,
-                       settingsCustomizer: SettingsPanelCustomizer<*>?) : RfsConnectionTestingBase<KafkaConnectionData>(project,
-                                                                                                                        settingsCustomizer)
+class KafkaTestingBase(
+  project: Project,
+  settingsCustomizer: SettingsPanelCustomizer<KafkaConnectionData>?
+) : RfsConnectionTestingBase<KafkaConnectionData>(project, settingsCustomizer)
