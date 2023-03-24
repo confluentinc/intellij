af457350669d4e9f710d2504286862217698cd8f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-24 15:08:47 +0100

    use komihash for icon classes to align rasterized key with a runtime cache key (same algo)
    GitOrigin-RevId: fec04acb91d4a69d5c50571d3408c9b0ce3fddd4

--- src/icons/BigdatatoolsKafkaIcons.java
+++ src/icons/BigdatatoolsKafkaIcons.java
@@ -13,8 +13,8 @@ public final class BigdatatoolsKafkaIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, BigdatatoolsKafkaIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Bookmark_off = load("icons/bookmark_off.svg", 2071958187, 0);
-  /** 16x16 */ public static final @NotNull Icon Bookmark_on = load("icons/bookmark_on.svg", 1739320840, 0);
-  /** 16x16 */ public static final @NotNull Icon Kafka = load("icons/kafka.svg", 530310811, 0);
-  /** 13x13 */ public static final @NotNull Icon KafkaToolWindow = load("icons/kafkaToolWindow.svg", 2045665598, 2);
+  /** 16x16 */ public static final @NotNull Icon Bookmark_off = load("icons/bookmark_off.svg", 30837841, 0);
+  /** 16x16 */ public static final @NotNull Icon Bookmark_on = load("icons/bookmark_on.svg", 1543176622, 0);
+  /** 16x16 */ public static final @NotNull Icon Kafka = load("icons/kafka.svg", -711445120, 0);
+  /** 13x13 */ public static final @NotNull Icon KafkaToolWindow = load("icons/kafkaToolWindow.svg", -1107798207, 2);
 }
