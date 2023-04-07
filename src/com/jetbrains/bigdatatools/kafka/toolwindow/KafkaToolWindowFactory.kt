5343577cf98cc2ffb7e0dde3f31bef5b6ab3036c
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2023-04-07 21:25:25 +0400

    [bdt] BDIDE-4020 Core: Extract Kafka and RFS plugin
    GitOrigin-RevId: 486ed553fe490380c40a1a40d1fbcd60437b024f

--- src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaToolWindowFactory.kt
+++ src/com/jetbrains/bigdatatools/kafka/toolwindow/KafkaToolWindowFactory.kt
@@ -3,13 +3,20 @@ package com.jetbrains.bigdatatools.kafka.toolwindow
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.jetbrains.bigdatatools.common.constants.BdtConnectionType
+import com.jetbrains.bigdatatools.common.constants.BdtPlugins
 import com.jetbrains.bigdatatools.common.monitoring.toolwindow.MonitoringToolWindowFactory
+import com.jetbrains.bigdatatools.common.settings.manager.RfsConnectionDataManager
 import com.jetbrains.bigdatatools.kafka.util.KafkaMessagesBundle
 
 class KafkaToolWindowFactory : MonitoringToolWindowFactory() {
   override val controllerId = KafkaMonitoringToolWindowController.TOOL_WINDOW_ID
   override val connectionGroupId = BdtConnectionType.KAFKA.id
   override val title: String = KafkaMessagesBundle.message("toolwindow.title")
+
+  override fun shouldBeAvailable(project: Project): Boolean {
+    return (BdtPlugins.isKafkaPluginInstalled()) &&
+           !RfsConnectionDataManager.instance?.getConnectionsByGroupId(connectionGroupId, project)?.filter { it.isEnabled }.isNullOrEmpty()
+  }
 
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     KafkaMonitoringToolWindowController.getInstance(project)?.setUp(toolWindow)
