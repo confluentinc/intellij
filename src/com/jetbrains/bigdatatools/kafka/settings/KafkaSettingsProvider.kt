f73b0eb4e4618b9bd1ed9f3faacc7f9eb5752b1b
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2023-04-10 12:07:14 +0400

    [bdt] BDIDE-4020 Core(fix): Fix Kafka plugin settings visibility
    GitOrigin-RevId: 76e4aa0114fda60a6b0de8cd02eade416042f123

--- src/com/jetbrains/bigdatatools/kafka/settings/KafkaSettingsProvider.kt
+++ src/com/jetbrains/bigdatatools/kafka/settings/KafkaSettingsProvider.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.bigdatatools.kafka.settings
 
+import com.jetbrains.bigdatatools.common.constants.BdtPluginType
 import com.jetbrains.bigdatatools.common.settings.connections.ConnectionGroup
 import com.jetbrains.bigdatatools.common.settings.connections.ConnectionSettingProvider
 
 class KafkaSettingsProvider : ConnectionSettingProvider {
+  override val pluginType: BdtPluginType = BdtPluginType.KAFKA
+
   override fun createConnectionGroups(): List<ConnectionGroup> = listOf(KafkaConnectionGroup())
 }
