9dd3dd659109553acaa2a1b54bd9bfba1e1f4800
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2023-04-18 11:31:15 +0400

    [bdt] BDIDE-4260 Kafka(fix): Dispose kafka client if error on checkConnection to avoid a lot of message in log
    GitOrigin-RevId: 2148c96b663874977f7e57415fe12eb59ac4f9ba

--- src/com/jetbrains/bigdatatools/kafka/client/KafkaClientBuilder.kt
+++ src/com/jetbrains/bigdatatools/kafka/client/KafkaClientBuilder.kt
@@ -3,6 +3,7 @@ package com.jetbrains.bigdatatools.kafka.client
 import com.jetbrains.bigdatatools.common.settings.kerberos.BdtKerberosManager
 import org.apache.kafka.clients.CommonClientConfigs.SECURITY_PROTOCOL_CONFIG
 import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.common.KafkaException
 import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.config.SaslConfigs.GSSAPI_MECHANISM
 import org.apache.kafka.common.security.auth.SecurityProtocol
@@ -14,7 +15,12 @@ object KafkaClientBuilder {
       BdtKerberosManager.instance.validateKrb5()
       BdtKerberosManager.instance.setupKerberosValues()
     }
-    return BdtKafkaAdminClient(AdminClient.create(properties))
+    return try {
+      BdtKafkaAdminClient(AdminClient.create(properties))
+    }
+    catch (t: KafkaException) {
+      throw t.cause ?: t
+    }
   }
 
   private val Properties.isKerberosEnabled
