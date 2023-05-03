44b5ecebbff43aafedc49cab1ddf6ca39501fac8
Confluent Eng <dtx-eng@confluent.io>
2023-05-03 10:53:47 +0300

    [bdt] Kafka(feat): Generation data for AVRO
    GitOrigin-RevId: ba1c5ac1772a59887e8215d5de350b3396db4ee6

--- src/com/jetbrains/bigdatatools/kafka/util/generator/GenerateRandomData.kt
+++ src/com/jetbrains/bigdatatools/kafka/util/generator/GenerateRandomData.kt
@@ -2,14 +2,16 @@ package com.jetbrains.bigdatatools.kafka.util.generator
 
 import com.jetbrains.bigdatatools.kafka.common.models.FieldType
 import com.jetbrains.bigdatatools.kafka.consumer.models.ConsumerProducerFieldConfig
+import com.jetbrains.bigdatatools.kafka.data.KafkaDataManager
 
 object GenerateRandomData {
-  fun generate(config: ConsumerProducerFieldConfig): String = when (config.type) {
+  fun generate(config: ConsumerProducerFieldConfig, dataManager: KafkaDataManager): String = when (config.type) {
     FieldType.STRING -> PrimitivesGenerator.generateString()
     FieldType.LONG -> PrimitivesGenerator.generateLong()
     FieldType.DOUBLE -> PrimitivesGenerator.generateDouble()
     FieldType.FLOAT -> PrimitivesGenerator.generateFloat()
     FieldType.BASE64 -> PrimitivesGenerator.generateBytes()
+    FieldType.AVRO_REGISTRY -> AvroGenerator.generateAvroMessage(config, dataManager)
     else -> ""
   }
 }
