09711a7240a7a76b27cff387b11edf593762bdbf
Confluent Eng <dtx-eng@confluent.io>
2023-05-18 16:51:49 +0300

    [bdt] Kafka(fix): Map, array and enum schema more detailed for Avro
    GitOrigin-RevId: 59bf38d58a61a176b98fd81b2c08753b2434a1d4

--- src/com/jetbrains/bigdatatools/kafka/registry/schema/AvroSchemaTree.kt
+++ src/com/jetbrains/bigdatatools/kafka/registry/schema/AvroSchemaTree.kt
@@ -2,32 +2,37 @@ package com.jetbrains.bigdatatools.kafka.registry.schema
 
 import io.confluent.kafka.schemaregistry.avro.AvroSchema
 import org.apache.avro.Schema
+import org.apache.avro.Schema.Type
 import javax.swing.tree.DefaultMutableTreeNode
 
 class AvroSchemaTree(private val schema: AvroSchema) : SchemaTree {
   private fun buildAvroSchemaTree(parent: DefaultMutableTreeNode, fieldName: String, schema: Schema, field: Schema.Field? = null) {
-    val typeName = when (schema.type) {
-      Schema.Type.MAP -> "map<string, ${schema.valueType.type.getName().lowercase()}>"
-      Schema.Type.FIXED -> "fixed size=${schema.fixedSize}"
-      Schema.Type.ARRAY -> "array<${schema.elementType}>"
-      else -> schema.type.getName().lowercase()
-    }
-
     val child = if (field != null)
-      createMutableNode(fieldName, typeName, field.defaultVal(), field.doc())
-    else createMutableNode(fieldName, typeName)
+      createMutableNode(fieldName, schema.typeName(), field.defaultVal(), field.doc())
+    else createMutableNode(fieldName, schema.typeName())
 
     parent.add(child)
     addNestedTypes(child, schema)
   }
 
   private fun addNestedTypes(parent: DefaultMutableTreeNode, schema: Schema) = when (schema.type) {
-    Schema.Type.RECORD -> schema.fields.forEach { buildAvroSchemaTree(parent, it.name(), it.schema(), it) }
-    Schema.Type.UNION -> for ((index, schemaItem) in schema.types.withIndex()) {
+    Type.RECORD -> schema.fields.forEach { buildAvroSchemaTree(parent, it.name(), it.schema(), it) }
+    Type.UNION -> for ((index, schemaItem) in schema.types.withIndex()) {
       buildAvroSchemaTree(parent, "[$index]", schemaItem)
+    }
+    Type.MAP -> {
+      parent.add(createMutableNode("key", "string"))
+      parent.add(createMutableNode("value", schema.valueType.typeName()))
+    }
+    Type.ARRAY -> parent.add(createMutableNode("value", schema.elementType.typeName()))
+    Type.FIXED -> parent.add(createMutableNode("size", schema.fixedSize.toString()))
+    Type.ENUM -> schema.enumSymbols?.forEachIndexed { index, enum ->
+      parent.add(createMutableNode("[$index]", enum))
     }
     else -> {}
   }
+
+  private fun Schema.typeName() = this.type.getName().lowercase()
 
   override fun buildTree(root: DefaultMutableTreeNode) {
     val rawSchema = schema.rawSchema()
