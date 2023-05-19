6b287e79610dbf13d1591dd0b1693ffbf2e93a8f
Confluent Eng <dtx-eng@confluent.io>
2023-05-19 17:41:52 +0300

    [bdt] Kafka(fix): Show details of combined schema
    GitOrigin-RevId: 16444c82643f2dd58d070ae06eee9a4df926e61c

--- src/com/jetbrains/bigdatatools/kafka/registry/schema/JsonSchemaTree.kt
+++ src/com/jetbrains/bigdatatools/kafka/registry/schema/JsonSchemaTree.kt
@@ -8,46 +8,49 @@ class JsonSchemaTree(private val schema: JsonSchema) : SchemaTree {
   private lateinit var requiredFields: List<String>
 
   private fun buildJsonSchemaTree(parent: DefaultMutableTreeNode, fieldName: String, schema: Schema) {
-    val type = if (schema is ArraySchema && schema.allItemSchema != null)
-      "array<${resolveJsonFieldType(schema.allItemSchema)}>"
-    else
-      resolveJsonFieldType(schema)
-
-    val child = createMutableNode(fieldName, type, schema.defaultValue, schema.description,
+    val child = createMutableNode(fieldName, schema.resolveFieldType(), schema.defaultValue, schema.description,
                                   requiredFields.contains(fieldName))
     parent.add(child)
     when (schema) {
-      is ObjectSchema -> schema.propertySchemas?.forEach { buildJsonSchemaTree(child, it.key, it.value) }
-      is ArraySchema -> schema.itemSchemas?.let {
-        for ((index, value) in it.withIndex()) {
-          buildJsonSchemaTree(child, "[$index]", value)
+      is ObjectSchema -> schema.propertySchemas?.forEach {
+        buildJsonSchemaTree(child, it.key, it.value)
+      }
+      is CombinedSchema -> schema.subschemas.forEachIndexed { index, value ->
+        buildJsonSchemaTree(child, "[$index]", value)
+      }
+      is ArraySchema -> {
+        if (schema.allItemSchema != null) {
+          child.add(createMutableNode("value", schema.allItemSchema.resolveFieldType()))
+        }
+        else schema.itemSchemas?.forEachIndexed { index, value ->
+          child.add(createMutableNode("[$index]", value.resolveFieldType()))
         }
       }
       else -> {}
     }
   }
 
+  private fun Schema.resolveFieldType() = when (this) {
+    // TODO: CombinedSchema ConditionalSchema EmptySchema NotSchema ReferenceSchema
+    is NullSchema -> "null"
+    is ArraySchema -> "array"
+    is BooleanSchema, is TrueSchema, is FalseSchema -> "boolean"
+    is NumberSchema -> when {
+      this.requiresInteger() -> "integer"
+      this.isRequiresNumber -> "number"
+      else -> ""
+    }
+    is ObjectSchema -> "object"
+    is StringSchema -> "string"
+    is EnumSchema -> "enum"
+    is ConstSchema -> "const"
+    else -> ""
+  }
+
   override fun buildTree(root: DefaultMutableTreeNode) {
     val objectSchema = schema.rawSchema() as? ObjectSchema ?: return
     requiredFields = objectSchema.requiredProperties
-    objectSchema.propertySchemas?.forEach { buildJsonSchemaTree(root, it.key, it.value) }
-  }
 
-  companion object {
-    // TODO: CombinedSchema ConditionalSchema ConstSchema EmptySchema NotSchema ReferenceSchema
-    fun resolveJsonFieldType(schemaValue: Schema) = when (schemaValue) {
-      is NullSchema -> "null"
-      is ArraySchema -> "array"
-      is BooleanSchema, is TrueSchema, is FalseSchema -> "boolean"
-      is NumberSchema -> when {
-        schemaValue.requiresInteger() -> "integer"
-        schemaValue.isRequiresNumber -> "number"
-        else -> ""
-      }
-      is ObjectSchema -> "object"
-      is StringSchema -> "string"
-      is EnumSchema -> "enum"
-      else -> ""
-    }
+    objectSchema.propertySchemas?.forEach { buildJsonSchemaTree(root, it.key, it.value) }
   }
 }
