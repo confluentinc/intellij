be2170b26d37fbd487841f69f8b16ccb26ccddd9
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2023-08-10 23:39:28 +0300

    [bdt] BDIDE-4856 refactor connection settings form:
    avoid lazy initializer with no idea in which context component is initialized
    
    GitOrigin-RevId: 8624c22a0eac68bc3840baefc3ecc5dbbe600e58

--- src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionConfigurable.kt
+++ src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionConfigurable.kt
@@ -5,12 +5,11 @@ import com.intellij.openapi.project.Project
 import com.jetbrains.bigdatatools.common.settings.connections.ConnectionConfigurable
 import com.jetbrains.bigdatatools.kafka.rfs.KafkaConnectionData
 
-class KafkaConnectionConfigurable(connectionData: KafkaConnectionData, project: Project) :
-  ConnectionConfigurable<KafkaConnectionData>(connectionData, project, BigdatatoolsKafkaIcons.Kafka) {
-  private val settingsCustomizer: KafkaSettingsCustomizer
-    get() = KafkaSettingsCustomizer(project, connectionData, disposable)
-
+class KafkaConnectionConfigurable(
+  connectionData: KafkaConnectionData,
+  project: Project
+) : ConnectionConfigurable<KafkaConnectionData, KafkaSettingsCustomizer>(connectionData, project, BigdatatoolsKafkaIcons.Kafka) {
   override fun getHelpTopic() = "big.data.tools.kafka"
-  override fun createSettingsCustomizer() = settingsCustomizer
+  override fun createSettingsCustomizer() = KafkaSettingsCustomizer(project, connectionData, disposable)
   override fun createConnectionTesting() = KafkaTestingBase(project, settingsCustomizer)
 }
