fd28b28115e0ffad329e6586253d5ee21e1804ed
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2023-08-13 12:32:07 +0300

    [bdt] BDIDE-4856 do not read/write credentials on driver creation: fixup after rebase
    GitOrigin-RevId: 52f7b676c0bbc09620a03babbb8f8604c39397a8

--- src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionConfigurable.kt
+++ src/com/jetbrains/bigdatatools/kafka/settings/KafkaConnectionConfigurable.kt
@@ -10,6 +10,6 @@ class KafkaConnectionConfigurable(
   project: Project
 ) : ConnectionConfigurable<KafkaConnectionData, KafkaSettingsCustomizer>(connectionData, project, BigdatatoolsKafkaIcons.Kafka) {
   override fun getHelpTopic() = "big.data.tools.kafka"
-  override fun createSettingsCustomizer() = KafkaSettingsCustomizer(project, connectionData, disposable)
+  override fun createSettingsCustomizer() = KafkaSettingsCustomizer(project, connectionData, disposable, coroutineScope)
   override fun createConnectionTesting() = KafkaTestingBase(project, settingsCustomizer)
 }
