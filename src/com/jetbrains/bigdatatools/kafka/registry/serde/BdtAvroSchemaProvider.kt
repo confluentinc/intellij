f3c10f071c6b82f5362dce90812db5a2e94f5d85
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2023-09-15 17:18:54 +0300

    [bdt] BDIDE-5082 Kafka(fix): SchemaParseException: Cannot parse <null> schema
    GitOrigin-RevId: 6bc98f24e84fdaad4d248b2f17141af0353c6af4

--- src/com/jetbrains/bigdatatools/kafka/registry/serde/BdtAvroSchemaProvider.kt
+++ src/com/jetbrains/bigdatatools/kafka/registry/serde/BdtAvroSchemaProvider.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.bigdatatools.kafka.registry.serde
 
 import io.confluent.kafka.schemaregistry.ParsedSchema
+import io.confluent.kafka.schemaregistry.avro.AvroSchema
 import io.confluent.kafka.schemaregistry.avro.AvroSchemaProvider
 import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference
 import org.apache.avro.SchemaParseException
@@ -9,10 +10,13 @@ class BdtAvroSchemaProvider : AvroSchemaProvider() {
   override fun parseSchemaOrElseThrow(schemaString: String?,
                                       references: MutableList<SchemaReference>?,
                                       isNew: Boolean): ParsedSchema = try {
-    super.parseSchemaOrElseThrow(schemaString, references, isNew)
+    AvroSchema(schemaString, references, resolveReferences(references), null, isNew)
   }
   catch (e: SchemaParseException) {
     val message = e.message?.replace("<", "&lt;")?.replace(">", "&gt;")
     throw SchemaParseException(message)
+  }
+  catch (e: Exception) {
+    throw e
   }
 }
