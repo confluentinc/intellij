9b1f092172db5afb7a22fa71d1b4b01d76179053
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2023-09-24 19:02:56 +0300

    [bdt] BDIDE-5132 Common(fix): Support columns without name for headers
    GitOrigin-RevId: 41c64f49196dbcdb5f38217f831d3e172ab8a30b

--- src/com/jetbrains/bigdatatools/kafka/util/KafkaLocalizedField.kt
+++ src/com/jetbrains/bigdatatools/kafka/util/KafkaLocalizedField.kt
@@ -4,6 +4,6 @@ import com.jetbrains.bigdatatools.common.monitoring.data.model.RemoteInfo
 import com.jetbrains.bigdatatools.common.monitoring.table.extension.LocalizedField
 import kotlin.reflect.KProperty1
 
-class KafkaLocalizedField<T : RemoteInfo>(field: KProperty1<T, *>, i18Key: String) : LocalizedField<T>(field, i18Key) {
-  override fun getLocalizedName() = KafkaMessagesBundle.message(i18Key)
+class KafkaLocalizedField<T : RemoteInfo>(field: KProperty1<T, *>, i18Key: String?) : LocalizedField<T>(field, i18Key) {
+  override fun getLocalizedName() = i18Key?.let { KafkaMessagesBundle.message(it) } ?: ""
 }
