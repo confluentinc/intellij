3e7cea53607c8b61fdcb546fbe16ee2db3159f44
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-10-31 20:57:23 +0200

    [kafka] IJPL-156867 Changed timepickers to simple textfield
    GitOrigin-RevId: 86b8add373c88653c64c275405c922a7d894ed7f

---
+++ src/com/jetbrains/bigdatatools/kafka/consumer/editor/TimestampTextField.kt
@@ -0,0 +1,53 @@
+package com.jetbrains.bigdatatools.kafka.consumer.editor
+
+import com.intellij.openapi.Disposable
+import com.jetbrains.bigdatatools.common.settings.buildValidator
+import com.jetbrains.bigdatatools.common.settings.registerValidator
+import com.jetbrains.bigdatatools.kafka.util.KafkaMessagesBundle
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.swing.JFormattedTextField
+import javax.swing.text.MaskFormatter
+
+internal class TimestampTextField(uiDisposable: Disposable) : JFormattedTextField(maskFormatter) {
+  private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").apply {
+    isLenient = false
+  }
+
+  init {
+    columns = 15
+    toolTipText = KafkaMessagesBundle.message("kafka.timestamp.tooltip.text")
+
+    val validator = buildValidator(
+      this,
+      { text },
+      {
+        val dateTime = getDateTime()
+        if (dateTime == null) KafkaMessagesBundle.message("kafka.timestamp.validation.error") else null
+      }
+    )
+    registerValidator(uiDisposable, validator, this)
+  }
+
+  fun getDateTime(): Date? = try {
+    dateFormat.parse(text)
+  }
+  catch (e: Exception) {
+    null
+  }
+
+  fun setDateTime(date: Any?) {
+    text = try {
+      dateFormat.format(date)
+    }
+    catch (_: Exception) {
+      ""
+    }
+  }
+
+  companion object {
+    private val maskFormatter = MaskFormatter("####-##-## ##:##:##").apply {
+      placeholderCharacter = '_'
+    }
+  }
+}
