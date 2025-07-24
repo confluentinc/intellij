01a1dfb8a5f3a82b67ba500da4e069b08aa48bba
Confluent Eng <dtx-eng@confluent.io>
2025-07-24 13:11:43 +0300

    [kafka] IJPL-191219 Fixed an exception when bdt connections loaded
    GitOrigin-RevId: 6a401c8f2b5ab6116477b6108333234b526e812a

--- src/com/jetbrains/bigdatatools/kafka/core/rfs/driver/ActivitySource.kt
+++ src/com/jetbrains/bigdatatools/kafka/core/rfs/driver/ActivitySource.kt
@@ -1,25 +1,10 @@
 package com.jetbrains.bigdatatools.kafka.core.rfs.driver
 
-import org.jetbrains.annotations.TestOnly
-
 enum class ActivitySource(val calledByUser: Boolean) {
   DEPEND_UPDATED(false),
   DRIVER_CREATION(false),
   TIMER(false),
-  ZEPPELIN_RECONNECT(false),
-  AWAIT_MONITORING(false),
-  ARBITRARY_CLUSTER_DEPENDENT(false),
-  EMR_DEPENDENT(false),
-  DATABRICKS_DELEGATE(false),
-
 
   ACTION(true),
   TEST_ACTION(true),
-  SFTP_CLONE_SUDO(true),
-  DATAPROC_DEPENDENT(true),
-  EMR_DEPENDENT_USER(true),
-  DATABRICKS_DELEGATE_USER(true),
-
-  @TestOnly
-  TEST(true),
 }
