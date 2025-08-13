b4e6638f68e8c01ff34a9e65ac88c6504b5b2d0d
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2025-08-13 11:56:43 +0300

    [kafka] IJ-CR-166799 Extracted library version to `libs.versions.toml`
    GitOrigin-RevId: ddd4d710f392e81d88ae36e5a6e793fa9259fecb

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,38 +45,34 @@ dependencies {
     testFramework(TestFrameworkType.Bundled)
     testFramework(TestFrameworkType.JUnit5)
   }
-  implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-
-  implementation("org.apache.kafka:kafka-clients:3.9.0")
+  implementation(libs.moshi.kotlin)
+  implementation(libs.kafka.clients)
 
   listOf(
-    "io.confluent:kafka-avro-serializer:7.2.0",
-    "io.confluent:kafka-json-schema-serializer:7.2.0",
-    "io.confluent:kafka-protobuf-serializer:7.2.0",
-    "io.confluent:kafka-schema-registry-client:7.2.0"
+    libs.kafka.avro.serializer,
+    libs.kafka.json.schema.serializer,
+    libs.kafka.protobuf.serializer,
+    libs.kafka.schema.registry.client
   ).forEach {
     implementation(it) {
       exclude("org.apache.kafka", "kafka-clients")
     }
   }
 
-  implementation("software.amazon.awssdk:apache-client:2.20.158")
-  implementation("software.amazon.awssdk:sso:2.20.158")
-  implementation("software.amazon.awssdk:sts:2.20.158")
-  implementation("software.amazon.awssdk:ssooidc:2.20.158")
+  implementation(libs.aws.apache.client)
+  implementation(libs.aws.sso)
+  implementation(libs.aws.sts)
+  implementation(libs.aws.ssooidc)
 
-  implementation("software.amazon.glue:schema-registry-serde:1.1.15")
+  implementation(libs.glue.schema.registry.serde)
+  implementation(libs.generex)
 
-  implementation("com.github.mifmif:generex:1.0.2")
-
-  // Tests
-  testImplementation("org.jetbrains.kotlinx", "kotlinx-metadata-jvm", "0.9.0")
-
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-  testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
-
-  testImplementation("junit:junit:4.13.2")
+  // tests
+  testImplementation(libs.kotlinx.metadata.jvm)
+  testImplementation(libs.junit.jupiter.api)
+  testRuntimeOnly(libs.junit.jupiter.engine)
+  testImplementation(libs.junit.jupiter.params)
+  testImplementation(libs.junit4)
 }
 
 sourceSets {
