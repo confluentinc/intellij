909fe2f452948abc77c3d98e32b9ee7ef5f41dc4
Bernie Chen <96664333+bcscc@users.noreply.github.com>
2025-09-29 16:24:29 -0500

    Fix CI cache key generation and update its TTL policy (#92)
    * Rename cache keys
    * Add cache key cleanup
    * Update cache TTL to 14 days
    * Print message for cache delete and test

--- mk-files/ci-build-cache.mk
+++ mk-files/ci-build-cache.mk
@@ -1,16 +1,19 @@
 # How many days cache entries can stay in the semaphore cache before they are considered stale
-SEM_CACHE_DURATION_DAYS ?= 7
+SEM_CACHE_DURATION_DAYS ?= 14
 current_time := $(shell date +"%s")
+gradle_checksum := $(shell checksum gradle.properties build.gradle.kts gradle/wrapper/gradle-wrapper.properties)
+sdkman_checksum := $(shell checksum .sdkmanrc)
 
 # This target stores two specific caches: Gradle dependencies and SDKMAN! installed SDKs.
 #
 # Logic adapted from https://github.com/confluentinc/ide-sidecar/blob/7640c2e752da8c28ae9e10f356e94b7331ded0e3/mk-files/semaphore.mk#L21-L54.
 #
-# A new cache is only stored if the previous one is older than `SEM_CACHE_DURATION_DAYS` (default 7 days).
-# Timestamp and the OS name in the cache key to prevent collisions and allow for fuzzy matching on restore.
+# A new cache is only stored if the previous one is older than `SEM_CACHE_DURATION_DAYS` (14 days).
+# Timestamp and the checksum of properties files in the cache key to prevent collisions and allow for fuzzy matching on restore.
 # Only write to the cache from main builds because of security reasons.
 .PHONY: ci-sem-cache-store
 ci-sem-cache-store: ci-sem-cache-store-gradle ci-sem-cache-store-sdkman
+	cache list --sort-by SIZE
 
 .PHONY: ci-sem-cache-restore
 ci-sem-cache-restore: ci-sem-cache-restore-gradle ci-sem-cache-restore-sdkman
@@ -22,14 +25,18 @@ ci-sem-cache-store-gradle:
 ifneq ($(SEMAPHORE_GIT_REF_TYPE),pull-request)
 	@echo "Storing Gradle-specific semaphore caches"
 	@set -e; \
-	current_checksum=$$(checksum gradle.properties build.gradle.kts gradle/wrapper/gradle-wrapper.properties); \
-	stored_key=$$(cache list | grep "gradle_caches_$$current_checksum" | awk '{print $$1}' | sort -r | awk 'NR==1'); \
+	stored_key=$$(cache list | grep "gradle_caches_$(gradle_checksum)" | awk '{print $$1}' | sort -r | awk 'NR==1'); \
 	stored_timestamp=$$(echo "$$stored_key" | awk -F_ '{print $$NF}'); \
 	threshold_timestamp=$$(date -d "$(SEM_CACHE_DURATION_DAYS) days ago" +%s); \
 	if [ -z "$$stored_timestamp" ] || [ "$$stored_timestamp" -lt "$$threshold_timestamp" ]; then \
-		echo "Gradle cache is too old or does not exist, storing it again..."; \
-		cache store "gradle_caches_$$current_checksum_$(current_time)" ~/.gradle/caches; \
-		cache store "gradle_wrapper_$$current_checksum_$(current_time)" ~/.gradle/wrapper; \
+		echo "Gradle cache is too old or does not exist"; \
+		echo "Cleaning up old gradle cache and wrapper keys..."; \
+		cache list | grep "^gradle_caches_" | awk '{print $$1}' | xargs -r -I {} sh -c 'echo "Deleting: $$0" && cache delete "$$0"' {}; \
+		cache list | grep "^gradle_wrapper_" | awk '{print $$1}' | xargs -r -I {} sh -c 'echo "Deleting: $$0" && cache delete "$$0"' {}; \
+		echo "Storing gradle_caches_$(gradle_checksum)_$(current_time)"; \
+		cache store "gradle_caches_$(gradle_checksum)_$(current_time)" ~/.gradle/caches; \
+		echo "Storing gradle_wrapper_$(gradle_checksum)_$(current_time)"; \
+		cache store "gradle_wrapper_$(gradle_checksum)_$(current_time)" ~/.gradle/wrapper; \
 	else \
 		echo "Gradle cache for this checksum was updated recently, skipping..."; \
 	fi
@@ -41,13 +48,15 @@ ci-sem-cache-store-sdkman:
 ifneq ($(SEMAPHORE_GIT_REF_TYPE),pull-request)
 	@echo "Storing SDKMAN! semaphore cache"
 	@set -e; \
-	current_checksum=$$(checksum .sdkmanrc); \
-	stored_key=$$(cache list | grep "sdkman_$$current_checksum" | awk '{print $$1}' | sort -r | awk 'NR==1'); \
+	stored_key=$$(cache list | grep "sdkman_$(sdkman_checksum)" | awk '{print $$1}' | sort -r | awk 'NR==1'); \
 	stored_timestamp=$$(echo "$$stored_key" | awk -F_ '{print $$NF}'); \
 	threshold_timestamp=$$(date -d "$(SEM_CACHE_DURATION_DAYS) days ago" +%s); \
 	if [ -z "$$stored_timestamp" ] || [ "$$stored_timestamp" -lt "$$threshold_timestamp" ]; then \
-		echo "SDKMAN! cache is too old or does not exist, storing it again..."; \
-		cache store "sdkman_$$current_checksum_$(current_time)" ~/.sdkman; \
+		echo "SDKMAN! cache is too old or does not exist"; \
+		echo "Cleaning up old sdkman cache key..."; \
+		cache list | grep "^sdkman_" | awk '{print $$1}' | xargs -r -I {} sh -c 'echo "Deleting: $$0" && cache delete "$$0"' {}; \
+		echo "Storing sdkman_$(sdkman_checksum)_$(current_time)"; \
+		cache store "sdkman_$(sdkman_checksum)_$(current_time)" ~/.sdkman; \
 	else \
 		echo "SDKMAN! cache for this checksum was updated recently, skipping..."; \
 	fi
@@ -57,14 +66,14 @@ endif
 .PHONY: ci-sem-cache-restore-gradle
 ci-sem-cache-restore-gradle:
 	@echo "Restoring Gradle-specific semaphore caches"
-	cache restore "gradle_caches_$(shell checksum gradle.properties build.gradle.kts gradle/wrapper/gradle-wrapper.properties)"
-	cache restore "gradle_wrapper_$(shell checksum gradle.properties build.gradle.kts gradle/wrapper/gradle-wrapper.properties)"
+	cache restore "gradle_caches_$(gradle_checksum)"
+	cache restore "gradle_wrapper_$(gradle_checksum)"
 
 # This target restores the SDKMAN! installed SDKs.
 .PHONY: ci-sem-cache-restore-sdkman
 ci-sem-cache-restore-sdkman:
 	@echo "Restoring SDKMAN! semaphore cache"
-	cache restore "sdkman_$(shell checksum .sdkmanrc)"
+	cache restore "sdkman_$(sdkman_checksum)"
 
 # Override the store-test-results-to-semaphore target to handle Gradle test results
 .PHONY: store-test-results-to-semaphore
