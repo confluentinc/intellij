324ad4ea5cb1a7b8d133cabca730884e9e00f80b
Bernie Chen <bchen@confluent.io>
2025-10-24 14:45:52 -0400

    Add identify event call upon plugin install and uninstall

--- src/io/confluent/intellijplugin/telemetry/Telemetry.kt
+++ src/io/confluent/intellijplugin/telemetry/Telemetry.kt
@@ -2,17 +2,16 @@ package io.confluent.intellijplugin.telemetry
 
 /**
  * Wrapper function for sending telemetry data to Segment.
- * see @TelemetryService for implementation.
- * see @TelemetryEvent for event definitions.
+ * @param TelemetryEvent The type of usage event with defined accompanied properties.
  */
 fun logUsage(event: TelemetryEvent) {
     TelemetryService.getInstance().trackEvent(event.eventName, event.properties())
 }
 
 /**
- * Send an identify event to Segment. Should only send once per user on plugin startup and again if user traits change.
+ * Wrapper function for sending identify event to Segment. Should only send once per user on plugin startup and again if user traits change.
  * TODO: Add traits to track
  */
-fun sendIdentifyEvent(traits: Map<String, Any>) {
+fun logUser(traits: Map<String, Any>) {
     TelemetryService.getInstance().sendIdentifyEvent(traits)
 }
