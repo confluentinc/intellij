eeccd2b3485b276de1df6fa2eaa8e08d38f5ddd4
Bernie Chen <96664333+bcscc@users.noreply.github.com>
2025-11-03 15:33:40 -0600

    Create application service to add Segment analytics SDK (2/3) (#114)
    * Add application level configuration for usage telemetry settings
    * Send telemetry events for `Connection Action`, `Action Invoked` and plugin lifecycle (3/3) (#116)

---
+++ src/io/confluent/intellijplugin/telemetry/Telemetry.kt
@@ -0,0 +1,36 @@
+package io.confluent.intellijplugin.telemetry
+
+import io.confluent.intellijplugin.rfs.KafkaConnectionData
+
+/**
+ * Wrapper function for sending telemetry data to Segment.
+ * @param TelemetryEvent The type of usage event with defined accompanied properties.
+ */
+fun logUsage(event: TelemetryEvent) {
+    TelemetryService.getInstance().sendTrackEvent(event.eventName, event.properties())
+}
+
+/**
+ * Wrapper function for sending identify event to Segment. Should only send once per user on plugin startup and again if user traits change.
+ * TODO: Add traits to track
+ */
+fun logUser(traits: Map<String, Any>) {
+    TelemetryService.getInstance().sendIdentifyEvent(traits)
+}
+
+/**
+ * Utility functions for telemetry tracking.
+ */
+
+/**
+ * Determines the authentication method used for a Kafka connection.
+ */
+fun determineAuthMethod(connectionData: KafkaConnectionData): String {
+    val properties = connectionData.secretProperties
+    return when {
+        properties.contains("sasl.mechanism") -> "SASL"
+        properties.contains("ssl.keystore") -> "SSL"
+        connectionData.anonymous -> "Anonymous"
+        else -> "None"
+    }
+}
