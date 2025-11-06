cf7c4fd88afa4b6907e650f38d0788ad925482af
Ellie Gui <egui@confluent.io>
2025-11-06 12:15:55 -0600

    Update Confluent Kafka libraries and fixed API errors (#140)

--- src/io/confluent/intellijplugin/registry/serde/BdtJsonSchemaProvider.kt
+++ src/io/confluent/intellijplugin/registry/serde/BdtJsonSchemaProvider.kt
@@ -1,21 +1,21 @@
 package io.confluent.intellijplugin.registry.serde
 
 import io.confluent.kafka.schemaregistry.ParsedSchema
-import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference
+import io.confluent.kafka.schemaregistry.client.rest.entities.Schema
 import io.confluent.kafka.schemaregistry.json.JsonSchema
 import io.confluent.kafka.schemaregistry.json.JsonSchemaProvider
 
 class BdtJsonSchemaProvider : JsonSchemaProvider() {
     override fun parseSchemaOrElseThrow(
-        schemaString: String?,
-        references: MutableList<SchemaReference>?,
-        isNew: Boolean
+        schema: Schema,
+        isNew: Boolean,
+        normalize: Boolean
     ): ParsedSchema =
         try {
             JsonSchema(
-                schemaString,
-                references,
-                resolveReferences(references),
+                schema.schema,
+                schema.references,
+                resolveReferences(schema),
                 null
             )
         } catch (e: Exception) {
--- src/io/confluent/intellijplugin/registry/serde/BdtProtobufSchemaProvider.kt
+++ src/io/confluent/intellijplugin/registry/serde/BdtProtobufSchemaProvider.kt
@@ -1,21 +1,21 @@
 package io.confluent.intellijplugin.registry.serde
 
 import io.confluent.kafka.schemaregistry.ParsedSchema
-import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference
+import io.confluent.kafka.schemaregistry.client.rest.entities.Schema
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchemaProvider
 
 class BdtProtobufSchemaProvider : ProtobufSchemaProvider() {
     override fun parseSchemaOrElseThrow(
-        schemaString: String?,
-        references: MutableList<SchemaReference>?,
-        isNew: Boolean
+        schema: Schema,
+        isNew: Boolean,
+        normalize: Boolean
     ): ParsedSchema =
         try {
             ProtobufSchema(
-                schemaString,
-                references,
-                resolveReferences(references),
+                schema.schema,
+                schema.references,
+                resolveReferences(schema),
                 null,
                 null
             )
