dfc8a6ad378804b3f659edb22c7502f2c205f40e
Ellie Gui <egui@confluent.io>
2025-11-06 12:15:55 -0600

    Update Confluent Kafka libraries and fixed API errors (#140)

--- src/io/confluent/intellijplugin/registry/serde/BdtAvroSchemaProvider.kt
+++ src/io/confluent/intellijplugin/registry/serde/BdtAvroSchemaProvider.kt
@@ -3,16 +3,16 @@ package io.confluent.intellijplugin.registry.serde
 import io.confluent.kafka.schemaregistry.ParsedSchema
 import io.confluent.kafka.schemaregistry.avro.AvroSchema
 import io.confluent.kafka.schemaregistry.avro.AvroSchemaProvider
-import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference
+import io.confluent.kafka.schemaregistry.client.rest.entities.Schema
 import org.apache.avro.SchemaParseException
 
 class BdtAvroSchemaProvider : AvroSchemaProvider() {
     override fun parseSchemaOrElseThrow(
-        schemaString: String?,
-        references: MutableList<SchemaReference>?,
-        isNew: Boolean
+        schema: Schema,
+        isNew: Boolean,
+        normalize: Boolean
     ): ParsedSchema = try {
-        AvroSchema(schemaString, references, resolveReferences(references), null, isNew)
+        AvroSchema(schema.schema, schema.references, resolveReferences(schema), null, isNew)
     } catch (e: SchemaParseException) {
         val message = e.message?.replace("<", "&lt;")?.replace(">", "&gt;")
         throw SchemaParseException(message)
