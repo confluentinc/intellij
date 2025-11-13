7a3547ff1da8be93df644882efc4072e2d765d97
Bernie Chen <bchen@confluent.io>
2025-11-13 20:44:04 +0000

    Update call

--- build.gradle.kts
+++ build.gradle.kts
@@ -166,7 +166,7 @@ afterEvaluate {
         (it.name.startsWith("sentry") || it.name.contains("Sentry")) && 
         it.name != "generateSentryConfig" 
     }.forEach { sentryTask ->
-        sentryTask.mustRunAfter(generateSentryConfig)
+        sentryTask.mustRunAfter(generateSentryConfig, generateSegmentConfig)
     }
 }
 
