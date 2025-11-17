5504f4fe131daa103ce9b658d72c2c614372cbd3
Ellie Gui <egui@confluent.io>
2025-11-17 16:57:39 -0600

    Add default Sentry tags for IDE, plugin, and platform metadata (#183)
    * added sentry tags including IDE, Plugin, Platform info
    
    * improve and fix some tags
    
    * make getPluginVersion() in a shared TelemetryUtils file
    
    * fix CI error
    
    * use PermanentInstallationID instead of host name
    
    * remove unused imports

--- src/io/confluent/intellijplugin/telemetry/SentryClient.kt
+++ src/io/confluent/intellijplugin/telemetry/SentryClient.kt
@@ -1,22 +1,60 @@
 package io.confluent.intellijplugin.telemetry
 
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.application.PermanentInstallationID
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.util.SystemInfo
 import io.sentry.Sentry
 
 object SentryClient {
     private val logger = Logger.getInstance(SentryClient::class.java)
-    
-    // Static initialization - runs when class is first accessed
+
     init {
         try {
             logger.info("Initializing Sentry")
             Sentry.init { options ->
                 options.dsn = SentryConfig.DSN
                 options.isDebug = false
+                options.release = TelemetryUtils.getPluginVersion()
+                options.serverName = getDeviceId()
+                options.setBeforeSend { event, _ ->
+                    addDefaultTags(event)
+                    event
+                }
             }
             logger.info("Sentry initialized successfully")
         } catch (e: Exception) {
             logger.error("Sentry initialization failed", e)
+        }
+    }
+
+    private fun addDefaultTags(event: io.sentry.SentryEvent) {
+        val appInfo = ApplicationInfo.getInstance()
+
+        event.setTag("productName", appInfo.fullApplicationName)
+        event.setTag("productVersion", appInfo.fullVersion)
+        event.setTag("pluginVersion", TelemetryUtils.getPluginVersion())
+        event.setTag("ide.build", appInfo.build.asString())
+        event.setTag("platform", getPlatformName())
+        event.setTag("arch", SystemInfo.OS_ARCH)
+        event.setTag("os", "${SystemInfo.OS_NAME} ${SystemInfo.OS_VERSION}")
+    }
+
+    private fun getPlatformName(): String {
+        return when {
+            SystemInfo.isMac -> "darwin"
+            SystemInfo.isWindows -> "win32"
+            SystemInfo.isLinux -> "linux"
+            else -> SystemInfo.OS_NAME.lowercase()
+        }
+    }
+
+    // Use anonymous device ID to avoid PII 
+    private fun getDeviceId(): String {
+        return try {
+            PermanentInstallationID.get()
+        } catch (e: Exception) {
+            "unknown"
         }
     }
 
