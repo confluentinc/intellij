060e13ad8cc8a33c56817b94ebcdfec37618f3f6
Bernie Chen <bchen@confluent.io>
2026-01-12 16:13:19 -0600

    Add import

--- test/io/confluent/intellijplugin/telemetry/TelemetryUtilsTest.kt
+++ test/io/confluent/intellijplugin/telemetry/TelemetryUtilsTest.kt
@@ -5,6 +5,7 @@ import com.intellij.testFramework.junit5.TestApplication
 import io.confluent.intellijplugin.settings.app.KafkaPluginSettings
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
