69daae791dda02175b1f8f22a208996a30656a42
Bernie Chen <bchen@confluent.io>
2026-02-20 10:59:02 -0600

    Revert

--- src/io/confluent/intellijplugin/settings/KafkaConnectionGroup.kt
+++ src/io/confluent/intellijplugin/settings/KafkaConnectionGroup.kt
@@ -4,8 +4,6 @@ import io.confluent.intellijplugin.icons.BigdatatoolsKafkaIcons
 import io.confluent.intellijplugin.core.constants.BdtConnectionType
 import io.confluent.intellijplugin.core.settings.connections.BrokerConnectionGroup
 import io.confluent.intellijplugin.core.settings.connections.ConnectionFactory
-import io.confluent.intellijplugin.rfs.KafkaCloudType
-import io.confluent.intellijplugin.rfs.KafkaConfigurationSource
 import io.confluent.intellijplugin.rfs.KafkaConnectionData
 
 class KafkaConnectionGroup : ConnectionFactory<KafkaConnectionData>(
@@ -17,7 +15,5 @@ class KafkaConnectionGroup : ConnectionFactory<KafkaConnectionData>(
     override fun newData() = KafkaConnectionData(version = 5).apply {
         name = BdtConnectionType.KAFKA.connName
         uri = "127.0.0.1:9092"
-        brokerConfigurationSource = KafkaConfigurationSource.CLOUD
-        brokerCloudSource = KafkaCloudType.CONFLUENT
     }
 }
