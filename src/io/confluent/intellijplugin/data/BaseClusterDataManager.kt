9144eef4f28dca3678d23c7dd76f95490ed7177a
Bernie Chen <96664333+bcscc@users.noreply.github.com>
2026-03-09 13:39:22 -0500

    Enable preset and details panel for CCloud consumer (#430)

--- src/io/confluent/intellijplugin/data/BaseClusterDataManager.kt
+++ src/io/confluent/intellijplugin/data/BaseClusterDataManager.kt
@@ -161,8 +161,9 @@ abstract class BaseClusterDataManager(
 
     abstract fun supportsConsumerGroups(): Boolean
     abstract fun supportsAdvancedSettings(): Boolean
-    abstract fun supportsPresets(): Boolean
-    abstract fun supportsDetailsPanel(): Boolean
+
+    // Flag for filtering saved presets depending on CCloud ("ccloud") vs Native (null) connection type
+    open fun presetConnectionTag(): String? = null
 
     val consumerPanelStorage: KafkaConsumerPanelStorage by lazy {
         KafkaConsumerPanelStorage(this).also { Disposer.register(this, it) }
