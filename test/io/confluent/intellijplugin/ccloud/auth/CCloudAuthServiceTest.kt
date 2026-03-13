fe0081ace3cd52c008c1c00e8a732079e703b436
Bernie Chen <bchen@confluent.io>
2026-03-13 10:26:50 -0500

    Add notification for token expiry

--- test/io/confluent/intellijplugin/ccloud/auth/CCloudAuthServiceTest.kt
+++ test/io/confluent/intellijplugin/ccloud/auth/CCloudAuthServiceTest.kt
@@ -393,6 +393,7 @@ class CCloudAuthServiceTest {
         fun setUpSpy() {
             spyService = spy(CCloudAuthService(CoroutineScope(SupervisorJob())))
             doNothing().whenever(spyService).showSignOutNotification()
+            doNothing().whenever(spyService).showSessionExpiredNotification()
             doNothing().whenever(spyService).showSignInSuccessNotification(any())
         }
 
@@ -490,6 +491,39 @@ class CCloudAuthServiceTest {
 
             verify(listener, times(1)).onSignedOut()
             verify(spyService, times(1)).showSignOutNotification()
+        }
+
+        @Test
+        fun `should show session expired notification when reason is session_expired`() {
+            spyService.context = createMockAuthenticatedContext()
+
+            spyService.signOut(reason = "session_expired")
+            SwingUtilities.invokeAndWait {}
+
+            verify(spyService).showSessionExpiredNotification()
+            verify(spyService, never()).showSignOutNotification()
+        }
+
+        @Test
+        fun `should show session expired notification when reason is refresh_failed`() {
+            spyService.context = createMockAuthenticatedContext()
+
+            spyService.signOut(reason = "refresh_failed")
+            SwingUtilities.invokeAndWait {}
+
+            verify(spyService).showSessionExpiredNotification()
+            verify(spyService, never()).showSignOutNotification()
+        }
+
+        @Test
+        fun `should show regular sign out notification for user initiated sign out`() {
+            spyService.context = createMockAuthenticatedContext()
+
+            spyService.signOut(reason = "user_initiated")
+            SwingUtilities.invokeAndWait {}
+
+            verify(spyService).showSignOutNotification()
+            verify(spyService, never()).showSessionExpiredNotification()
         }
     }
 
