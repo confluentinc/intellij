c14cb27e8469a28b59b3dd6726a32e479f491054
eguicf <egui@confluent.io>
2026-04-08 16:15:21 -0500

    unused imports

--- src/io/confluent/intellijplugin/telemetry/SentryClient.kt
+++ src/io/confluent/intellijplugin/telemetry/SentryClient.kt
@@ -1,7 +1,6 @@
 package io.confluent.intellijplugin.telemetry
 
 import com.intellij.openapi.application.ApplicationInfo
-import com.intellij.openapi.application.PermanentInstallationID
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.SystemInfo
 import io.sentry.Sentry
